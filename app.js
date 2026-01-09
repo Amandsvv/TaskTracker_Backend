b7c4566ab142cc0e5069c594bd53a84fc444d2d1
Aman Kumar <amandsvv4006@gmail.com>
2026-01-10 00:51:13 +0530

    Url changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import taskRoutes from './routes/taskRoutes.js';
 const app = express();
 
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : process.env.FRONTEND_URL || "http://localhost:5173",
   credentials :true
 }));
 
